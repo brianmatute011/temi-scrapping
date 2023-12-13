76f4935dfc3dc532a2c72f7a922fc8edb2df4ea6
Brian Matute <37722161+brianmatute011@users.noreply.github.com>
2023-12-12 21:56:49 -0500

    Update pdftables-api dependency
    + pdftables-api dependency from GitHub

--- tlibs/PDF_Scrapper_Api/setup.py
+++ tlibs/PDF_Scrapper_Api/setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pdf_scrapper_api',
-    version='0.1',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.18.0',
         'pandas>=1.0.0',
         'PyPDF2>=1.0.0',
-        'shutil',
-        'git+https://github.com/pdftables/python-pdftables-api.git',
         'PyMuPDF>=1.18.14', # Esta es la dependencia para fitz (PyMuPDF es el nombre del paquete que incluye fitz)
+        'pdftables-api @ https://github.com/pdftables/python-pdftables-api/archive/master.tar.gz'
     ],
 )
