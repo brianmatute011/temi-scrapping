e98d459d850d0b4e186db277f949f15be6949eea
Brian Matute <37722161+brianmatute011@users.noreply.github.com>
2023-12-12 20:09:44 -0500

    new features available
    + Refactoring
    + Process and import option
    + Design adaptable to window size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout
+from PyQt6.QtGui import QStandardItemModel
 from app.temi import Ui_MainWindow
 from qtlibs.utilstemi import open_file_dialog
 
@@ -10,7 +11,34 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.actionOpen_file.triggered.connect(open_file_dialog)
 
+        self.setup_layouts()
+        
+    def setup_layouts(self):
+        central_widget = self.ui.centralwidget
 
+        # Get the widgets from the generated file
+        scroll_area = self.ui.scrollArea
+        push_button = self.ui.pushButton
+        push_button_2 = self.ui.pushButton_2
+        label = self.ui.label
+        label_2 = self.ui.label_2
+
+        # Create layouts and establish them
+        main_layout = QVBoxLayout(central_widget)
+        bottom_buttons_layout = QHBoxLayout()
+        top_labels = QHBoxLayout()
+
+        main_layout.addLayout(top_labels)       
+        main_layout.addWidget(scroll_area)
+        main_layout.addLayout(bottom_buttons_layout)
+        
+
+        bottom_buttons_layout.addWidget(push_button)
+        bottom_buttons_layout.addWidget(push_button_2)
+
+        top_labels.addWidget(label)
+        top_labels.addWidget(label_2)
+               
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
